1139658593c01eee6bf2edffd64d4c32b14df4e2
Yongje Shin <yongje1211@gmail.com>
2023-06-08 23:06:33 +0900

    [unrated] Title: 공배수, Time: 0.03 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = 0;
-        
-        if ((number % n) == 0) {
-            if ((number % m) == 0) {
-                answer = 1;
-            }
-        }
-        
-        return answer;
+        return number % n == 0 && number % m == 0 ? 1 : 0;
     }
 }
