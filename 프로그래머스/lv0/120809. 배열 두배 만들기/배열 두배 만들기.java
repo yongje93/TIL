8123e3361b3bfd24843fafaba98853b8da5e9558
Yongje Shin <yongje1211@gmail.com>
2023-06-13 23:44:50 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        int i = 0;
+        for (int num : numbers) {
+            answer[i] = num*2;
+            i++;
+        }
+        return answer;
+    }
+}
