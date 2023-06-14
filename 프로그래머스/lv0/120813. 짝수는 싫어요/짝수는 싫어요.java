ae0c703721fbb43f9c1db511fe10550a66501305
Yongje Shin <yongje1211@gmail.com>
2023-06-14 23:31:53 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = (n%2 == 0) ? n/2 : n/2 + n%2;
+        int[] answer = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            answer[i] = (2*i) + 1;
+        }
+        return answer;
+    }
+}
