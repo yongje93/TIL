9ed90567aeb35778f9aaff1a157671fa2b6928c7
Yongje Shin <yongje1211@gmail.com>
2023-06-15 23:21:12 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (String str1 : s1) {
+            for (String str2 : s2) {
+                if (str2.equals(str1)) {
+                    answer++;
+                }
+            }
+        }
+            
+        return answer;
+    }
+}
