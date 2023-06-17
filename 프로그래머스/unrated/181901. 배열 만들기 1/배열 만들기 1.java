a85d1ebe4ef39d439a5f0be0d34969b6dccbfbfb
Yongje Shin <yongje1211@gmail.com>
2023-06-17 23:27:04 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,14 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 1; i <= n; i++) {
-            if (i % k == 0) {
-                list.add(i);
-            }
+        int count = n / k;
+
+        int[] answer = new int[count];
+
+        for (int i = 1; i <= count; i++) {
+            answer[i - 1] = k * i;
         }
-        return list.stream().mapToInt(i -> i).toArray();
+
+        return answer;
     }
 }
