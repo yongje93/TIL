11b9eaf35d841560816cf141802c9c4b91b23952
Yongje Shin <yongje1211@gmail.com>
2023-06-22 22:50:23 +0900

    [unrated] Title: rny_string, Time: 0.09 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        return rny_string.contains("m") ? rny_string.replaceAll("m", "rn") : rny_string;
+        return rny_string.replaceAll("m", "rn");
     }
 }
