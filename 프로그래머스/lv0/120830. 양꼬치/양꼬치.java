6f9de1bf19d7e489c0de4f29ba7b3e2df8c22f03
Yongje Shin <yongje1211@gmail.com>
2023-06-22 22:45:52 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        int free = n / 10;
-        answer = n * 12000 + (k - free) * 2000;
-        return answer;
+        return n * 12000 + (k - n / 10) * 2000;
     }
 }
