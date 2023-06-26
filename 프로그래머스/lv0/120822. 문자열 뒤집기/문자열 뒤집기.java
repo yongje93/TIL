05cbf51dc830a7ce897c266d190406aada3dba48
Yongje Shin <yongje1211@gmail.com>
2023-06-26 17:25:16 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String my_string) {    
-        char[] arr = my_string.toCharArray();
-        char[] reversedArr = new char[arr.length];
-        for(int i = 0; i < arr.length; i++) {
-            reversedArr[arr.length-1-i] = arr[i];
-        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
 
-        return new String(reversedArr);
+        return sb.toString();
     }
 }
