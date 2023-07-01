12e10e5925ef44bcb75f9b41e05a6f8c08b0c770
Yongje Shin <yongje1211@gmail.com>
2023-07-01 17:48:57 +0900

    [level 0] Title: 암호 해독, Time: 1.49 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        for (int i = 1; i <= cipher.length(); i++) {
-            if (i % code == 0) {
-                answer = answer + cipher.charAt(i - 1) + "";
-            }
+        
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
         }
+        
         return answer;
     }
 }
