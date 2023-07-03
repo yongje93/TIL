eb6f03583029dd823e32bb00ecade338fb814d09
Yongje Shin <yongje1211@gmail.com>
2023-07-03 23:13:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.65 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String my_string) {
-        my_string = my_string.replaceAll("[a-z]", "");
-        
-        String[] arr = my_string.split("");
-        int[] answer = new int[arr.length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            answer[i] = Integer.parseInt(arr[i]);
+        my_string = my_string.replaceAll("[a-z]","");
+
+        int[] answer = new int[my_string.length()];
+
+        for(int i =0; i<my_string.length(); i++){
+            answer[i] = my_string.charAt(i) - '0';
         }
+
         Arrays.sort(answer);
-        
+
         return answer;
     }
 }
