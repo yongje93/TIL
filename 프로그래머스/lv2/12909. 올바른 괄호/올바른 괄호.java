a1c7094a28b6835b66470b712ad6f22872b7747d
Yongje Shin <yongje1211@gmail.com>
2023-07-06 22:14:47 +0900

    [level 2] Title: 올바른 괄호, Time: 17.43 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.size() == 0) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (stack.size() != 0) answer = false;
+        
+        return answer;
+    }
+}
