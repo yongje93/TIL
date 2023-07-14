9761c0fbf2c5745e9112541c2b18c0fdeb0b5ab2
Yongje Shin <yongje1211@gmail.com>
2023-07-14 21:01:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         for (char c : my_string.toCharArray()) {
-            if (c >= 48 && c <= 57) {
+            if (c >= '0' && c <= '9') {
                 answer += c - '0';
             }
         }
