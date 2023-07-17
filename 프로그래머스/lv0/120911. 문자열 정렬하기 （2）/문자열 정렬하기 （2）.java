4dff301483f3e362163f81d283241857ca93a451
Yongje Shin <yongje1211@gmail.com>
2023-07-17 21:42:42 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.40 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        char[] arr = answer.toCharArray();
+        Arrays.sort(arr);
+        return String.valueOf(arr);
+    }
+}
