8459dbaeacf1b4e52dd54b598d5e6f2ca7c51107
Yongje Shin <yongje1211@gmail.com>
2023-08-09 22:19:40 +0900

    [unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer.toArray(new String[0]);
+    }
+}
