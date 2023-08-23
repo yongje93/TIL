c9dd2337b8c59ccb21edb7382b38ca7b8f6bbe6f
Yongje Shin <yongje1211@gmail.com>
2023-08-23 22:57:34 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,12 +1,11 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] names) {
-        List<String> result = new ArrayList<>();
-        result.add(names[0]);
-        for (int i = 5; i < names.length; i+=5) {
-            result.add(names[i]);
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        int idx = 0;
+        for (int i = 0; i < names.length; i+=5) {
+            answer[idx] = names[i];
+            idx++;
         }
-        return result.stream().toArray(String[]::new);
+        return answer;
     }
 }
