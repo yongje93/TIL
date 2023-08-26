329ca92e3b25ec616714451ecbee0a502153f427
Yongje Shin <yongje1211@gmail.com>
2023-08-26 18:23:45 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,15 +1,13 @@
 class Solution {
     public int solution(int n) {
         int answer = 1;
-        int check = 1;
-        int i = 2;
-
-        while (check <= n) {
-            check *= i;
-            answer = i;
-            i++;
+        int factorial = 1;
+        
+        while(n >= factorial) {
+            answer ++;
+            factorial *= answer;
         }
-
-        return answer - 1;
+        
+        return answer -1 ;
     }
 }
