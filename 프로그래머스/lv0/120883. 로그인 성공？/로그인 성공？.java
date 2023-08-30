a9ded921c2f652d609ce2c00b086888e0e25a916
Yongje Shin <yongje1211@gmail.com>
2023-08-30 22:31:59 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -5,7 +5,6 @@ class Solution {
             if (id_pw[0].equals(data[0])) {
                 if (id_pw[1].equals(data[1])) {
                     answer = "login";
-                    break;
                 } else {
                     answer = "wrong pw";
                 }
