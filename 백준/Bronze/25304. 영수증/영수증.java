9dfd76260736b1efc57af9131fbc6398361d8a5b
Yongje Shin <yongje1211@gmail.com>
2024-01-30 22:49:50 +0900

    [Bronze IV] Title: 영수증, Time: 228 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            sum += a * b;
+        }
+
+        if (sum == X) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+}
