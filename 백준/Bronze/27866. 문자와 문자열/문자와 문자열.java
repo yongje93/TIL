cb3ac9f48569d19558d1bbc9c352d93ea741e85a
Yongje Shin <yongje1211@gmail.com>
2024-02-01 23:15:10 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.next();
+        int i = scanner.nextInt();
+
+        System.out.println(s.charAt(i - 1));
+    }
+
+}
